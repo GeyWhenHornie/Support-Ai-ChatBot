05d1375d9f1f0a1be336c5ea8ff7380823054455
Shubham Raj <168171242+shubhcoding01@users.noreply.github.com>
2024-12-27 17:49:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAhEeHNhBau8cLzrcvYslhis6VqSGyBhBk"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
 
 let user={
     message:null,
